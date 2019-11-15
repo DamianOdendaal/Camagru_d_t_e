47bd012fcde401cd3e94789141e906d3787341b2
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-15 12:58:30 +0200

    making things neat

--- setup.php
+++ setup.php
@@ -1,16 +1,8 @@
 <?php
     session_start();
     include ("connect.php");
-    include ("database.php");
-    // try {
-    //     $db = new PDO("mysql:host=localhost", $DB_USER, $DB_PASSWORD);
-    // }
-    // catch (exception $e)
-    // {
-    //     echo "An error has occured";
-    // }
-    // $db->exec("CREATE DATABASE IF NOT EXISTS camagru_users");
-    // $db->exec("CREATE TABLE IF NOT EXISTS camagru_users.users (ID INT AUTO_INCREMENT, Username VARCHAR(255), Password VARCHAR(255), Email VARCHAR(255), Token TEXT(8), Status VARCHAR(10), Connection VARCHAR(20), PRIMARY KEY(ID))");
+	include ("database.php");
+	
     try 
 	{
 		$conn = new PDO($DB_SERVER, $DB_USER, $DB_PASSWORD);
@@ -36,11 +28,9 @@
         
 		$sql = "CREATE TABLE IF NOT EXISTS images 
 		(
-			image_id INT(255) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-			user_id INT(255) NOT NULL,
-			FOREIGN KEY (user_id) REFERENCES users(user_id), 
-			image LONGTEXT CHARACTER SET utf8 NOT NULL,
-			date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL	
+			ID INT(255) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+			Username VARCHAR(255) UNIQUE NOT NULL,
+			FOREIGN KEY (Username) REFERENCES users(Username), 
 		)";
 		$conn->exec($sql);
 		echo "The images table was successfully created<br/>";
@@ -49,7 +39,7 @@
 		$sql = "CREATE TABLE IF NOT EXISTS comments
 		(
 			Comment TEXT NOT NULL,
-			Username INT(255) NOT NULL,
+			Username VARCHAR(255) UNIQUE NOT NULL,
 			FOREIGN KEY (Username) REFERENCES users(Username),
 			Image VARCHAR(255) NOT NULL
 		)";
@@ -59,7 +49,7 @@
 
 		$sql = "CREATE TABLE IF NOT EXISTS likes
 		(
-			Username INT(255) NOT NULL,
+			Username VARCHAR(255) UNIQUE NOT NULL,
 			FOREIGN KEY (Username) REFERENCES users(Username),
 			Image VARCHAR(255) NOT NULL
 		)";
